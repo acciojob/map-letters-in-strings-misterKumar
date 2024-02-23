f0fbf556815bea237e05538f1b9182152e3afeaf
misterKumar <120304503+misterKumar@users.noreply.github.com>
2024-02-23 19:04:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+function mapLetters(str) {
+	let ans={};
+	for(let i=0;i<str.length;i++){
+		if(ans[str[i]]){
+			ans[str[i]].push(i);
+		}
+		else{
+			ans[str[i]]=[i];
+		}
+	}
+	return ans;
+}
+let str=prompt();
+let obj=mapLetters(str);
+console.log(obj);
